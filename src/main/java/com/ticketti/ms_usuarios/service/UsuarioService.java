429e0536f94dc11716472b8745eeb8ebc94e8cf1
IngridNunez <ing.nunez@duocuc.cl>
2026-04-28 21:01:44 -0400

    se agregó en el controller advice para manejo de errores, github workflows (Github actions), se quitó la encriptación por spring security para que la maneje la bd

--- src/main/java/com/ticketti/ms_usuarios/service/UsuarioService.java
+++ src/main/java/com/ticketti/ms_usuarios/service/UsuarioService.java
@@ -88,14 +88,9 @@ public class UsuarioService {
 		nuevoUsuario.setNombre(nuevoUsuario.getNombre().trim());
 		nuevoUsuario.setCorreo(correoNormalizado);
 		
-		//se  codifica la contraseña antes de cuardarla en la base de datos y no se guarde en texto plano
-		//aplicando el algoritmo de hashing bcrypt y spring security para generar un hash seguro de la contraseña del usuario
-		//la base de datos debe encriptar la contraseña no spring securty 
-		if (nuevoUsuario.getContrasena() != null && !nuevoUsuario.getContrasena().trim().isEmpty()) {
-			nuevoUsuario.setContrasena(contrasenaEncoder.encode(nuevoUsuario.getContrasena()));
-		}
 
-		
+		//la base de datos debe encriptar la contraseña no spring securty 
+	
 
 		return usuarioRepository.save(nuevoUsuario);
 	}
